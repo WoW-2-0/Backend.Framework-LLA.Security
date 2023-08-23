62254df3123348e2095df07c7a38ed784c30e702
Max <sultonbek.rakhimov@gmail.com>
2023-08-23 13:25:03 +0500

    Finished Identity with AAD example

---
+++ Identity.Cloud.AAD/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Identity.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddUserSecrets<Program>();
+
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(options => builder.Configuration.Bind("AzureAd", options));
+
+builder.Services.AddControllers(options =>
+{
+    var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+    options.Filters.Add(new AuthorizeFilter(policy));
+});
+
+var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/", () => "Hi!");
+app.MapGet("/about", () => "This is the about page.").RequireAuthorization();
+
+app.Run();
